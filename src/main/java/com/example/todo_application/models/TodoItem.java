8470ecffd59276a2d3fc6ed1c26131eff0c3f073
don99ishara <donishara99@gmail.com>
2025-03-23 17:48:48 +0530

    Springboot task web app

---
+++ src/main/java/com/example/todo_application/models/TodoItem.java
@@ -0,0 +1,72 @@
+package com.example.todo_application.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+
+@Entity
+@Table(name = "todo_items")
+public class TodoItem implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String description;
+
+    private Boolean isComplete;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIsComplete() {
+        return isComplete;
+    }
+
+    public void setIsComplete(Boolean isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("TodoItem{id=%d, description='%s', isComplete='%s', createdAt='%s', updatedAt='%s'}",
+                id, description, isComplete, createdAt, updatedAt);
+    }
+}
+
---
+++ src/main/java/com/example/todo_application/repositories/TodoItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.todo_application.repositories;
+
+import com.example.todo_application.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+}
